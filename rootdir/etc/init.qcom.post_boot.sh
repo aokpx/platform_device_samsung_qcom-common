658a33dafda2ba94a5659924a391dc6cd5d9ce0c
Luis Cruz <ljc2491@gmail.com>
2013-04-30 15:09:51 -0500

    init: msm8960: set ondemand as default Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    Change-Id: I6755fcf07966e71397a097af518d1ae8e948ab77

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -115,28 +115,28 @@ case "$target" in
          echo 1 > /sys/devices/system/cpu/cpu1/online
          echo 1 > /sys/devices/system/cpu/cpu2/online
          echo 1 > /sys/devices/system/cpu/cpu3/online
-         #echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-         #echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-         #echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-         #echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-         #echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-         #echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-         #echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-         #echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-         #echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
-         #echo 70 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
-         #echo 3 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential_multi_core
-         #echo 918000 > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
-         #echo 1026000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
-         #echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
-         #chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-         #chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-         #chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-         echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-         echo "interactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-         echo "interactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-         echo "interactive" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-         echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+         echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+         echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+         echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+         echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+         echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+         echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+         echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+         echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+         echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
+         echo 70 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
+         echo 3 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential_multi_core
+         echo 918000 > /sys/devices/system/cpu/cpufreq/ondemand/optimal_freq
+         echo 1026000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
+         echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
+         chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+         chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+         chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+         #echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+         #echo "interactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+         #echo "interactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+         #echo "interactive" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+         #echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
          echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
          echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
          echo 384000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
