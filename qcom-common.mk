324227b462562b162911323239d258f036c354b3
Shareef Ali <shareefalis@cyanogenmod.org>
2013-05-19 17:09:07 -0500

    qcom-common: add common qcom ril class

--- qcom-common.mk
+++ qcom-common.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.composition.type=dyn \
     persist.hwc.mdpcomp.enable=true \
-    debug.mdpcomp.logs=0
+    debug.mdpcomp.logs=0 \
+    ro.telephony.ril_class=SamsungQualcommRIL
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     PRODUCT_PROPERTY_OVERRIDES += debug.mdpcomp.maxlayer=3
